d5a2bb500721ae0366c07bad096be9f9c89ce00c
Gaël Leconte <gaelleconte@Gaels-MacBook-Pro.local>
2022-03-14 20:28:43 -0400

    gael

---
+++ app/src/main/java/com/example/project_uqac/ui/ArticlesAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.project_uqac.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.project_uqac.R
+
+
+class ArticlesAdapter (private val mArticles: List<Article>) : RecyclerView.Adapter<ArticlesAdapter.ViewHolder>()
+{
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // Your holder should contain and initialize a member variable
+        // for any view that will be set as you render a row
+        val titleTextView: TextView = itemView.findViewById<TextView>(R.id.itemTitre)
+        val lieuTextView: TextView = itemView.findViewById<TextView>(R.id.itemLieu)
+        val dateTextView: TextView = itemView.findViewById<TextView>(R.id.itemDate)
+        val descriptionTextView: TextView = itemView.findViewById<TextView>(R.id.itemDescription)
+    }
+
+    // ... constructor and member variables
+    // Usually involves inflating a layout from XML and returning the holder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticlesAdapter.ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val articleView = inflater.inflate(R.layout.item_search, parent, false)
+        // Return a new holder instance
+        return ViewHolder(articleView)
+    }
+
+    // Involves populating data into the item through holder
+    override fun onBindViewHolder(viewHolder: ArticlesAdapter.ViewHolder, position: Int) {
+        // Get the data model based on position
+        val article: Article = mArticles[position]
+        // Set item views based on your views and data model
+        val titleView = viewHolder.titleTextView
+        titleView.text = article.title
+        val lieuView = viewHolder.lieuTextView
+        lieuView.text = article.lieu
+        val dateView = viewHolder.dateTextView
+        dateView.text = article.date
+        val descriptionView = viewHolder.descriptionTextView
+        descriptionView.text = article.description
+
+    }
+
+    // Returns the total count of items in the list
+    override fun getItemCount(): Int {
+        return mArticles.size
+    }
+}
