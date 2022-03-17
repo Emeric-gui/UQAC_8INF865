bee8cb3defcf6c8de06c4f48bd117ece93b83ee8
SkYiMlTo <gogo.bourreau@hotmail.fr>
2022-03-17 15:50:58 -0400

    My account

--- app/src/main/java/com/example/project_uqac/ui/my_account/MyAccountFragment.kt
+++ app/src/main/java/com/example/project_uqac/ui/my_account/MyAccountFragment.kt
@@ -4,17 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.project_uqac.databinding.FragmentMyAccountBinding
-import com.example.project_uqac.ui.my_account.MyAccountViewModel
+import com.example.project_uqac.databinding.FragmentLoginBinding
 
 class MyAccountFragment : Fragment() {
 
     private lateinit var dashboardViewModel: MyAccountViewModel
-    private var _binding: FragmentMyAccountBinding? = null
+    private var _binding: FragmentLoginBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,14 +25,9 @@ class MyAccountFragment : Fragment() {
         dashboardViewModel =
             ViewModelProvider(this).get(MyAccountViewModel::class.java)
 
-        _binding = FragmentMyAccountBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
 
-        val textView: TextView = binding.textMyAccount
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
